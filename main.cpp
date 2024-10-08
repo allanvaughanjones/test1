a524a50b53422af04aae590dac9f10b9b5a99d47
Allan <allan@vallumtech.com>
2024-10-08 09:45:07 +0100

    Added files!

---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <regex>
+#include <vector>
+#include <algorithm>
+#include <mutex>
+#include <chrono>
+#include <future>
+#include <iostream>
+#include <list>
+#include <numeric>
+#include <thread>
+#include <fstream>
+#include <vector>
+#include <fmt/format.h>
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json;
+
+#include <algorithm>
+#include <errno.h>
+#include <poll.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/inotify.h>
+#include <unistd.h>
+#include <string.h>
+#include <iostream>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sstream>
+#include <regex>
+#include <set>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <crypt.h>
+#include <pwd.h>
+#include <sys/types.h>
+#include <experimental/filesystem>
+#include <typeinfo>
+
+
+
+int main(int argc, char* argv[])
+{
+	(void)argc; (void)argv;
+
+}
